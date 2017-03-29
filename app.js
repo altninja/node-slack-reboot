98614f20039f7a6039f7f73955b9a85b9859af98
altninja <benny.gervais@gmail.com>
2017-03-29 21:20:12 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const 
+	express = require('express'),
+	app = express(),
+	request = require('request'),
+	bodyParser = require('body-parser'),
+	sys = require('sys'),
+	exec = require('child_process').exec;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', (process.env.PORT || 9001));
+
+app.get('/', (req, res) => {
+	res.send('Node-Slack-Rebooter9000XXL Online');
+});
+
+app.post('/reboot', (req, res) => {
+	exec("reboot", (error) => {
+		if (error !== null) {
+			var body = {
+				response_type: "in_channel",
+				text: 'exec error: ' + error
+			};
+			res.send(body);	
+		} else {
+			var body = {
+				response_type: "in_channel",
+				text: 'Rebooting Dev Server'
+		  	};
+			res.send(body);	
+		}
+	});
+});
+
+app.listen(app.get('port'), () => {
+	console.log('Node-Slack-Reboot is running on port', app.get('port'));
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+### NODE SLACK REBOOT
+For rebooting dev server via Slack slash command
