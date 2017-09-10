19fd0fb250b4cbf2d92d850b9a6a2f103f821423
Ben Gervais <benny.gervais@gmail.com>
2017-09-10 15:24:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,34 @@ const
 	request = require('request'),
 	bodyParser = require('body-parser'),
 	sys = require('sys'),
-	exec = require('child_process').exec;
+	exec = require('child_process').exec
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('port', (9001));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('port', (9001))
 
 app.get('/', (req, res) => {
-	res.send('Node-Slack-Rebooter9000XXL Online');
-});
+	res.send('Node Slack Reboot Online');
+})
 
 app.post('/reboot', (req, res) => {
+	// Command on server here - you can make this a shell script or whatever too
 	exec("/sbin/reboot", (error) => {
 		if (error !== null) {
 			var body = {
 				response_type: "in_channel",
 				text: 'exec error: ' + error
-			};
-			res.send(body);	
+			}
+		} else {
+			var body = {
+				response_type: "in_channel",
+				text: 'Reboot Success'
+			}
 		}
-	});
-});
+		res.send(body)
+	})
+})
 
 app.listen(app.get('port'), () => {
-	console.log('Node-Slack-Reboot is running on port', app.get('port'));
-});
+	console.log('Node Slack Reboot is running on port', app.get('port'))
+})
